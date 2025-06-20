52407ee7d6c02fd76ff6fab8cbd2f3d625bfb726
davidMZL <david.zea.27@unsch.edu.pe>
2025-06-20 00:36:51 -0500

    feat: add font icons locally, adjust list users

--- src/common/component/ListUsers.vue
+++ src/common/component/ListUsers.vue
@@ -1,58 +1,66 @@
 <script src="./ListUsers.ts" lang="ts"></script>
 
 <template>
-  <VCard class="pa-4 elevation-2 w-100">
-    <VRow no-gutters align="center">
-      <VAvatar size="50" color="primary" tile>
-        <VIcon dark>mdi-clipboard-text</VIcon>
-      </VAvatar>
-      <div class="ml-4 text-h5 font-weight-medium">
-        {{ nameComponent }}
+  <VContainer fluid class="px-4">
+    <VCard class="pa-4 elevation-2 mx-auto" width="1200" min-width="600" max-width="1200">
+      <VRow no-gutters align="center">
+        <VAvatar size="50" color="primary" tile>
+          <VIcon dark>mdi-clipboard-text</VIcon>
+        </VAvatar>
+        <div class="ml-4 text-h5 font-weight-medium">
+          {{ nameComponent }}
+        </div>
+        <VSpacer />
+      </VRow>
+
+      <VDivider class="my-4" />
+
+      <VTable
+        v-if="!isLoading"
+        class="elevation-1"
+        :height="users.length > 0 ? 'calc(100vh - 300px)' : 'auto'"
+        fixed-header
+      >
+        <thead>
+          <tr>
+            <th class="text-center">ID</th>
+            <th class="text-center">NOMBRE COMPLETO</th>
+            <th class="text-center">USUARIO</th>
+            <th class="text-center">EMAIL</th>
+            <th class="text-center">TELÉFONO</th>
+            <th class="text-center">ACCIONES</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(tempUser, index) in users" :key="index">
+            <td class="text-center">{{ tempUser.id }}</td>
+            <td class="text-center">{{ tempUser.name }}</td>
+            <td class="text-center">{{ tempUser.username }}</td>
+            <td class="text-center">{{ tempUser.email }}</td>
+            <td class="text-center">{{ tempUser.phone }}</td>
+            <td class="text-center">
+              <div class="d-flex justify-center">
+                <VTooltip text="Editar" top>
+                  <template #activator="{ props }">
+                    <VBtn color="info" variant="plain" size="x-small" v-bind="props">
+                      <VIcon icon="mdi-account-edit" size="22" />
+                    </VBtn>
+                  </template>
+                </VTooltip>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </VTable>
+
+      <div v-else class="d-flex justify-center align-center" style="height: 200px">
+        <div class="text-center">
+          <VProgressCircular indeterminate color="primary" size="40" />
+          <div class="mt-3">Cargando usuarios...</div>
+        </div>
       </div>
-
-      <VSpacer />
-    </VRow>
-
-    <VDivider class="my-4" />
-
-    <VTable
-      v-if="!isLoading"
-      class="elevation-1"
-      :height="users.length > 0 ? 'calc(100vh - 300px)' : 'auto'"
-      fixed-header
-    >
-      <thead>
-        <tr>
-          <th class="text-center">CÓDIGO</th>
-          <th class="text-center">NOMBRE COMPLETO</th>
-          <th class="text-center">USUARIO</th>
-          <th class="text-center">EMAIL</th>
-          <th class="text-center">TELÉFONO</th>
-          <th class="text-center">ACCIONES</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(tempUser, index) in users" :key="index">
-          <td class="text-center">{{ tempUser.id }}</td>
-          <td class="text-center">{{ tempUser.name }}</td>
-          <td class="text-center">{{ tempUser.username }}</td>
-          <td class="text-center">{{ tempUser.email }}</td>
-          <td class="text-center">{{ tempUser.phone }}</td>
-          <td class="text-center">
-            <div class="d-flex justify-center">
-              <VTooltip text="Editar" top>
-                <template #activator="{ props }">
-                  <VBtn color="info" variant="plain" size="x-small" v-bind="props">
-                    <VIcon icon="mdi-account-edit" size="22" />
-                  </VBtn>
-                </template>
-              </VTooltip>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </VTable>
-  </VCard>
+    </VCard>
+  </VContainer>
 </template>
 
 <style scoped></style>
--- src/views/users/UsersView.vue
+++ src/views/users/UsersView.vue
@@ -3,13 +3,7 @@ import ListUsers from '@/common/component/ListUsers.vue'
 </script>
 
 <template>
-  <VContainer fluid class="pa-6 px-0">
-    <VRow>
-      <VCol cols="12">
-        <ListUsers title="Listado de Usuarios" />
-      </VCol>
-    </VRow>
-  </VContainer>
+  <ListUsers title="Listado de Usuarios" />
 </template>
 
 <style scoped></style>
